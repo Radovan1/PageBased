ddd5e3aef72b9861c2b7c0b2fdee1e6969f163f5
Radovan Mojsovski <radovan_mojsovski@hotmail.com>
2017-12-03 12:59:32 +0100

    Initial commit

---
+++ PageBased/PageViewController.swift
@@ -0,0 +1,68 @@
+
+import UIKit
+
+protocol SwitchHighlightedButtonDelegate {
+    func highlight(buttonNumber: Int)
+}
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, SwitchPageProtocol, UIScrollViewDelegate {
+
+    var pages: [UIViewController]!
+    var currentIndex = 0
+    
+    var highlightDelegate: SwitchHighlightedButtonDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        for view in self.view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.delegate = self
+            }
+        }
+        
+        pages = [
+            (storyboard?.instantiateViewController(withIdentifier: "page1"))!,
+            (storyboard?.instantiateViewController(withIdentifier: "page2"))!,
+            (storyboard?.instantiateViewController(withIdentifier: "page3"))!
+        ]
+        
+        setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = pages.index(of: viewController) else { return nil }
+        let previousIndex = vcIndex - 1
+        guard previousIndex >= 0 else { return nil }
+        guard pages.count > previousIndex else { return nil }
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let vcIndex = pages.index(of: (viewControllers?.first)!)!
+        highlightDelegate?.highlight(buttonNumber: vcIndex)
+        currentIndex = vcIndex
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = pages.index(of: viewController) else { return nil }
+        let nextIndex = vcIndex + 1
+        guard pages.count != nextIndex else { return nil }
+        guard pages.count > nextIndex else { return nil }
+        return pages[nextIndex]
+    }
+    
+    func pageToSwitch(to page: Int) {
+        if page > currentIndex {
+            setViewControllers([pages[page]], direction: .forward, animated: true, completion: nil)
+            currentIndex = page
+            
+        } else if page < currentIndex {
+            setViewControllers([pages[page]], direction: .reverse, animated: true, completion: nil)
+            currentIndex = page
+        }
+    }
+}
---
+++ PageBased/ViewController.swift
@@ -0,0 +1,40 @@
+
+import UIKit
+
+protocol SwitchPageProtocol {
+    func pageToSwitch(to page: Int)
+}
+
+class ViewController: UIViewController, SwitchHighlightedButtonDelegate {
+
+    var switchPageDelegate: SwitchPageProtocol?
+    
+    var lineView: UIView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lineView = UIView(frame: CGRect(x: 0, y: 48, width: self.view.frame.width/3, height: 2))
+        lineView.backgroundColor = UIColor.red
+        
+        buttons.first?.addSubview(lineView)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let pageController = segue.destination as! PageViewController
+        switchPageDelegate = pageController
+        pageController.highlightDelegate = self
+    }
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        highlight(buttonNumber: sender.tag)
+        switchPageDelegate?.pageToSwitch(to: sender.tag)
+    }
+    
+    func highlight(buttonNumber: Int) {
+        lineView.removeFromSuperview()
+        buttons[buttonNumber].addSubview(lineView)
+    }
+}
